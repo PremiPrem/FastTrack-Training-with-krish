d63b4dc5e6850a393bb0f73bd6673d2fd40e61ad
PremiPrem <premisha1995@gmail.com>
2021-05-29 16:03:15 +0530

    changes added

--- Algorithms_array/WordCount/index.js
+++ Algorithms_array/WordCount/index.js
@@ -3,7 +3,8 @@ var str= "I am premisha";
 function count (str) {  
     var count = {};
     var removedspace= str.split(" ").join("");
-    removedspace.split('').forEach(function(s) {
+    var res=removedspace.toLowerCase();
+   res.split('').forEach(function(s) {
        count[s] ? count[s]++ : count[s] = 1;
     });
     return count;
